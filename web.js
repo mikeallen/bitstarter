cec658085acfff78442aaab565fcfa37af4e97d8
Mike Allen <mike.p.allen@gmail.com>
2014-01-12 22:17:33 -0800

    fixed use of readFileSync

--- web.js
+++ web.js
@@ -1,21 +1,18 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
-//var buffer = [];
 
 app.use(express.logger());
 
 var filePath = "index.html";
+var buffer = fs.readFileSync(filePath, {encoding: 'utf-8'}, function(err,data){
+        if (err){
+            throw(err);
+        }
+    });
 
 app.get('/', function(request, response) {
-    fs.readFileSync(filePath, {encoding: 'utf-8'}, function(err,data){
-        if (!err){
-            response.send(data);
-        }else{
-            console.log(err);
-        }
-
-    });
+    response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
