052386331f8719405b3be2ae7e8ca63e2e3ed434
Mike Allen <mike.p.allen@gmail.com>
2014-01-12 22:08:22 -0800

    changed to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var app = express();
+//var buffer = [];
+
 app.use(express.logger());
 
+var filePath = "index.html";
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    fs.readFileSync(filePath, {encoding: 'utf-8'}, function(err,data){
+        if (!err){
+            response.send(data);
+        }else{
+            console.log(err);
+        }
+
+    });
 });
 
 var port = process.env.PORT || 5000;
